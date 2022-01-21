e2418cb84cd68cf00615ce963de3140988af7b1a
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2022-01-21 00:07:24 -0300

    Preparação de estrutura do front

---
+++ client/src/pages/Carnet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Carnet() {
+ return (
+   <div>
+
+   </div>
+ );
+}
---
+++ client/src/pages/Pix/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Pix() {
+ return (
+   <div>
+
+   </div>
+ );
+}
