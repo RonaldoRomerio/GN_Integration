e2e47910b45dccbca8c92fcc996d06ee838cd511
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2022-01-22 20:46:26 -0300

    Inserção do component título com botão para mudança de tema

--- client/src/components/ButtonTheme/index.js
+++ client/src/components/ButtonTheme/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BiSun, BiMoon } from "react-icons/bi";
 import { Button } from './styled';
+import { ThemeContext } from '../../contexts/ThemeContext';
 export default function ButtonTheme() {
-  return (
-    <div>
-      <Button >
-        <BiSun size={25} color='#D65A31'/>
-      </Button>
-      <Button>
-        <BiMoon size={25}color='#D65A31'/>
-      </Button>
-    </div>
-  );
+
+  const{isDark, tonggleTheme} = useContext(ThemeContext);
+  if(isDark){
+    return (
+      <div>
+        <Button onClick={(() => tonggleTheme())}>
+          <BiSun size={25} color='#D65A31'/>
+        </Button>
+      </div>
+    );
+  }else{
+    return (
+      <div>
+        <Button onClick={(() => tonggleTheme())}>
+          <BiMoon size={25}color='#D65A31'/>
+        </Button>
+      </div>
+    );
+  }
+
 }
